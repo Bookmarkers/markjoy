54fe98ae33047c2c433c1102a2772ffe304ca157
Vicky Rodriguez <vrod8763@gmail.com>
2020-07-06 16:53:45 -0400

    update userSeed.js

---
+++ script/userSeed.js
@@ -0,0 +1,15 @@
+const faker = require('faker')
+faker.seed(123)
+
+const userSeed = []
+for (let i = 0; i < 100; i++) {
+  userSeed.push({
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: '123!',
+    inageUrl: '/icon/profile-icon.jpg'
+  })
+}
+
+module.exports = userSeed

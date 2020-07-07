b1ff1f906339c0a578511dd98e0ca7c28d98ae84
Yanjaa W <60143722+yanjaaw@users.noreply.github.com>
2020-07-07 17:59:23 -0400

    Apigoalsusers (#90)
    * feat: goal API routes made
    
    * feat: readme updated with naming convention table
    
    * feat: updated readme with naming table

---
+++ server/api/goals.js
@@ -0,0 +1,79 @@
+const router = require('express').Router()
+const {Goal} = require('../db/models')
+module.exports = router
+
+// Delete, update, create, get one goal. Get all goals.
+// Potentially should it be by user? I.e. '/userId:/id' where userId: req.params.userId and id is id?
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const goalToDelete = await Goal.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (goalToDelete) {
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const goalToUpdate = await Goal.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    if (goalToUpdate) {
+      res.status(200).json(goalToUpdate)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newGoal = await Goal.create(req.body)
+    if (newGoal) {
+      res.status(201).json(newGoal)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const goal = await Goal.findByPk(req.params.id)
+    if (goal) {
+      res.status(200).json(goal)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const goals = await Goal.findAll()
+    if (goals) {
+      res.status(200).json(goals)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})

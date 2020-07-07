1d5c3844f6a6a48d3c4a55a6c50b5373f0411f0b
Jianna Park <jiannajihyunpark@gmail.com>
2020-07-07 15:03:24 -0400

    add more bookmark routes

--- server/api/bookmarks.js
+++ server/api/bookmarks.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Bookmark} = require('../db/models')
+const {Bookmark, UserBookmark} = require('../db/models')
 module.exports = router
 
 // get all, get by id, update by id, delete by id, and create routes
@@ -16,11 +16,47 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
+// get all bookmarks with categoryId
+router.get('/category/:id', async (req, res, next) => {
+  try {
+    const bookmarks = await Bookmark.findAll({
+      where: {
+        categoryId: req.params.id
+      }
+    })
+    if (bookmarks) {
+      res.status(200).json(bookmarks)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// get all bookmarks with goalId
+router.get('/goal/:id', async (req, res, next) => {
+  try {
+    const bookmarks = await Bookmark.findAll({
+      where: {
+        goalId: req.params.id
+      }
+    })
+    if (bookmarks) {
+      res.status(200).json(bookmarks)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const bookmarks = await Bookmark.findAll()
     if (bookmarks) {
-      res.status(200).res.json(bookmarks)
+      res.status(200).json(bookmarks)
     } else {
       res.sendStatus(404)
     }
@@ -59,6 +95,20 @@ router.put('/:id', async (req, res, next) => {
   }
 })
 
+router.delete('/:userId/:bookmarkId', async (req, res, next) => {
+  try {
+    await UserBookmark.destroy({
+      where: {
+        userId: req.params.userId,
+        bookmarkId: req.params.bookmarkId
+      }
+    })
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.delete('/:id', async (req, res, next) => {
   try {
     const bookmarkToDelete = await Bookmark.destroy({
@@ -67,7 +117,7 @@ router.delete('/:id', async (req, res, next) => {
       }
     })
     if (bookmarkToDelete) {
-      res.status(202)
+      res.sendStatus(204)
     } else {
       res.sendStatus(404)
     }

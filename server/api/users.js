e7daecca9399547c051464097733ce13465555b3
Yanjaa <hi@yanjaa.com>
2020-07-08 12:19:48 -0400

    feat: store for goals

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,19 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    if (user) {
+      res.status(200).res.json(user)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -11,7 +24,7 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
-  } catch (err) {
-    next(err)
+  } catch (error) {
+    next(error)
   }
 })

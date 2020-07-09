1f8770ac974f6fb520ee5108df0a6654dd8b852c
Vicky Rodriguez <vrod8763@gmail.com>
2020-07-09 16:14:28 -0400

    delete BUGSSSgit add .

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {UserHome, AllBookmarks, AuthForm, Landing, AllGoals, SingleGoal} from './components'
+import {
+  UserHome,
+  AllBookmarks,
+  AuthForm,
+  Landing,
+  AllGoals,
+  SingleGoal
+} from './components'
 import {me} from './store'
 
 /**
@@ -24,7 +31,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route exact path="/home" component={UserHome} />
             <Route exact path="/bookmarks" component={AllBookmarks} />
             <Route exact path="/goals" component={AllGoals} />
             <Route exact path="/goals/:id" component={SingleGoal} />

01ac2662d2b582636e2ccd96bc7bf5a952cfe685
vjrodriguez <60943272+vjrodriguez@users.noreply.github.com>
2020-07-10 16:01:49 -0400

    Goal (#135)
    * update goal layout
    
    * cap goal
    
    * update layout
    
    * bookmarks can be fetched in allgoals
    
    * bookmarks are associated with user goals

--- client/components/all-goals.js
+++ client/components/all-goals.js
@@ -3,8 +3,13 @@ import {connect} from 'react-redux'
 // import {Link} from 'react-router-dom'
 import {fetchGoals, deleteGoal, updateGoal} from '../store/goal'
 import {me} from '../store/user'
-import {fetchBookmarks} from '../store/bookmark'
-import {Item, Button, Container, Form, Input, Segment} from 'semantic-ui-react'
+import {
+  fetchBookmarks,
+  fetchBookmarksByGoal,
+  fetchBookmarksByCategory
+} from '../store/bookmark'
+import {Item, Button, Container, Form, Input} from 'semantic-ui-react'
+import {getBookmarkByGoal} from '../../util'
 
 export class AllGoals extends React.Component {
   constructor(props) {
@@ -21,7 +26,8 @@ export class AllGoals extends React.Component {
   componentDidMount() {
     this.props.getGoals()
     this.props.getUser()
-    this.props.getBookmarks() //problem fetching bookmark!
+    this.props.getBookmarks()
+    //this.props.getBookmarksByGoal(goalId)
   }
 
   edit(goalId, prevDetail) {
@@ -36,7 +42,16 @@ export class AllGoals extends React.Component {
   render() {
     const goals = this.props.goals
     const user = this.props.user
-    //const bookmarks = this.props.bookmarks
+    const bookmarks = this.props.bookmarks
+    console.log('GOALS: ', goals)
+    console.log('BOOKMARKS: ', bookmarks)
+    const userGoals = goals.filter(goal => goal.userId === user.id)
+    const goalBookmarks = function(goalId, bookmarks) {
+      return bookmarks.filter(bookmark => {
+        return bookmark.goalId === goalId
+      })
+    }
+    console.log('CAN YOU SEE: ', goalBookmarks(9, bookmarks))
     return (
       <Container>
         <Item.Group relaxed>
@@ -45,7 +60,7 @@ export class AllGoals extends React.Component {
                 if (goal.userId === user.id)
                   return (
                     <Item key={goal.id}>
-                      <Item.Image size="small" src="./placeholder.png" />
+                      {/* <Item.Image size="small" src="./category/unsorted.png" /> */}
                       <Item.Content verticalAlign="middle">
                         {this.state.selectedGoalId === goal.id ? (
                           <Form
@@ -73,14 +88,24 @@ export class AllGoals extends React.Component {
                             {/* </Item.Extra> */}
                           </Form>
                         ) : (
-                          <Item.Header as="a">
+                          <Item.Header as="div">
                             GOAL: {goal.detail}
                             <Item.Extra>
                               <Item.Meta>
                                 Here's something to help with this goal: {'  '}
                               </Item.Meta>
                               <Item.Description>
-                                <a>www.nytimes.com</a>
+                                {goalBookmarks(goal.id, bookmarks)[0] ? (
+                                  <a
+                                    href={
+                                      goalBookmarks(goal.id, bookmarks)[0].url
+                                    }
+                                  >
+                                    {goalBookmarks(goal.id, bookmarks)[0].url}
+                                  </a>
+                                ) : (
+                                  'There are no bookmarks for this goal!'
+                                )}
                               </Item.Description>
                               <Button
                                 floated="right"
@@ -121,10 +146,9 @@ const mapDispatch = dispatch => {
     getGoals: () => dispatch(fetchGoals()),
     getUser: () => dispatch(me()),
     getBookmarks: () => dispatch(fetchBookmarks()),
-    updateGoal: (goalId, updateInfo) =>
-      dispatch(updateGoal(goalId, updateInfo)),
-    deleteGoal: goalId => dispatch(deleteGoal(goalId))
+    getBookmarksByGoal: goalId => dispatch(fetchBookmarksByGoal(goalId)),
+    deleteGoal: goalId => dispatch(deleteGoal(goalId)),
+    updateGoal: (goalId, updateInfo) => dispatch(updateGoal(goalId, updateInfo))
   }
 }
 export default connect(mapState, mapDispatch)(AllGoals)
-//export default AllGoals

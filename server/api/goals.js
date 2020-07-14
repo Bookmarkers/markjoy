88973b5a7a73c68e02bf0ab72eae1ccc53ecfe16
Vicky Rodriguez <vrod8763@gmail.com>
2020-07-14 15:32:02 -0400

    add checkIfUser access function

--- server/api/goals.js
+++ server/api/goals.js
@@ -5,6 +5,26 @@ module.exports = router
 // Delete, update, create, get one goal. Get all goals.
 // Potentially should it be by user? I.e. '/userId:/id' where userId: req.params.userId and id is id?
 
+const checkIfUser = async (req, res, next) => {
+  try {
+    const goal = await Goal.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    const userIdOnGoal = goal.dataValues.userId
+    console.log('What is the goal?', goal)
+    console.log('what is the userId on this goal?', userIdOnGoal)
+    if (req.user.dataValues.id !== userIdOnGoal) {
+      throw new Error('Goal not found!')
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+
 router.delete('/:id', async (req, res, next) => {
   try {
     const goalToDelete = await Goal.destroy({
@@ -52,24 +72,25 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.get('/user/:userId', async (req, res, next) => {
-  try {
-    const userGoals = await Goal.findAll({
-      where: {
-        userId: req.params.userId
-      }
-    })
-    if (userGoals) {
-      res.status(200).json(userGoals)
-    } else {
-      res.sendStatus(404)
-    }
-  } catch (err) {
-    next(err)
-  }
-})
+// router.get('/user/:userId', async (req, res, next) => {
+//   try {
+//     const userGoals = await Goal.findAll({
+//       where: {
+//         userId: req.params.userId
+//       }
+//     })
+//     if (userGoals) {
+//       res.status(200).json(userGoals)
+//     } else {
+//       res.sendStatus(404)
+//     }
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
-router.get('/:id', async (req, res, next) => {
+//goalId
+router.get('/:id', checkIfUser, async (req, res, next) => {
   try {
     const goal = await Goal.findByPk(req.params.id)
     if (goal) {
@@ -84,7 +105,11 @@ router.get('/:id', async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    const goals = await Goal.findAll()
+    const goals = await Goal.findAll({
+      where: {
+        userId: req.user.id
+      }
+    })
     if (goals) {
       res.status(200).json(goals)
     } else {

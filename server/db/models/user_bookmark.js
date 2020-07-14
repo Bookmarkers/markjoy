963aeb4c035e886b2feeced1dfad0ca16fe49998
Jianna Park <jiannajihyunpark@gmail.com>
2020-07-14 13:18:52 -0400

    remove user_bookmark model

--- server/db/models/user_bookmark.js
+++ server/db/models/user_bookmark.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+// const Sequelize = require('sequelize')
+// const db = require('../db')
 
-const UserBookmark = db.define('user_bookmark', {
-  note: {
-    type: Sequelize.TEXT
-  },
-  favorite: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
-  },
-  archived: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
-  }
-})
+// const UserBookmark = db.define('user_bookmark', {
+//   note: {
+//     type: Sequelize.TEXT
+//   },
+//   favorite: {
+//     type: Sequelize.BOOLEAN,
+//     defaultValue: false
+//   },
+//   archived: {
+//     type: Sequelize.BOOLEAN,
+//     defaultValue: false
+//   }
+// })
 
-module.exports = UserBookmark
+// module.exports = UserBookmark

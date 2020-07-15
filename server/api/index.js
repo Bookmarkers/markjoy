1589e2a831a5a254617e4e7b8ddf55364f415203
Vicky Rodriguez <vrod8763@gmail.com>
2020-07-15 15:25:26 -0400

    delete useless routes

--- server/api/index.js
+++ server/api/index.js
@@ -8,22 +8,6 @@ router.use('/bookmarks', require('./bookmarks'))
 router.use('/goals', require('./goals'))
 router.use('/blocked', require('./blocked'))
 
-router.get('/', (req, res, next) => {
-  try {
-    res.status(404).send('Access Denied!')
-  } catch (error) {
-    next(error)
-  }
-})
-
-// router.get('/bookmarks', (req, res, next) => {
-//   try {
-//    res.status(404).send('Access Denied!')
-//   } catch (error) {
-//     next(error)
-//   }
-// })
-
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404

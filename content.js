f949a910e4028c480303a6f1c4e00377e3a0ac78
Jianna Park <jiannajihyunpark@gmail.com>
2020-07-15 17:25:56 -0400

    get user and blockedUrl data to extension

--- content.js
+++ content.js
@@ -4,7 +4,10 @@
 //     if (text[i].innerHTML.includes('President Donald Trump')) {
 //         text[i].innerHTML = text[i].innerHTML.replace('President Donald Trump', 'The Dark Lord')
 // }
-const blockedUrls = ['twitter.com', 'instagram.com']
+// const blockedUrls = ['twitter.com', 'instagram.com']
+
+// const blockedUrls =
+// window.localStorage.setItem('blockedUrls', chrome.extension.getURL('main.js'))
 
 // Logic implementation: no one can block the redirect page cause it can cause infinite loop and doesn't make sense.
 // I.e. need to check if user is blocking our SPA page and/or the redirection page and not allow for it in the model?
@@ -21,7 +24,39 @@ const blockedUrls = ['twitter.com', 'instagram.com']
 // async await
 // pass in string for the gettree
 
-window.onload = function() {
+// const ourHost = "http://markjoy.herokuapp.com"
+const ourHost = 'http://localhost:8080'
+
+window.onload = async function() {
+  function fetchHappen() {
+    fetch(`${ourHost}/auth/me`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => data.id)
+      .then(userId => {
+        fetch(`${ourHost}/api/blocked/user/${userId}`, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => data.map(blocked => blocked.url))
+          .then(blockedUrls =>
+            window.localStorage.setItem(
+              'blockedUrls',
+              JSON.stringify(blockedUrls)
+            )
+          )
+      })
+  }
+
+  await fetchHappen()
+
   if (window.location.href.indexOf('twitter.com') > -1) {
     // if (blockedUrls.includes(window.location.href)) {
     window.location.replace('http://localhost:8080')

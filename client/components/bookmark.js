35a17ebbced5b65f6bcf15e047e15321e7245d35
vjrodriguez <60943272+vjrodriguez@users.noreply.github.com>
2020-07-17 15:18:04 -0400

    New popup url (#215)
    * full url on popup
    
    * update cleanUrl
    
    * cleanUrl and pop up
    
    * Update bookmark.js
    
    Co-authored-by: Jianna Park <52939452+jiannapark@users.noreply.github.com>

--- client/components/bookmark.js
+++ client/components/bookmark.js
@@ -6,7 +6,7 @@ import {
   addBookmark,
   deleteBookmark
 } from '../store/bookmark'
-import {Button, Image, List} from 'semantic-ui-react'
+import {Button, Image, List, Popup} from 'semantic-ui-react'
 import {Navbar} from './index'
 import {CustomSidebar} from './sidemenu'
 import AddBookmark from './create-or-update-bookmark'
@@ -21,22 +21,25 @@ const titles = {
   '/bookmarks/category/6': 'Unsorted'
 }
 
+const cleanUrl = url => {
+  if (url.length > 30) {
+    url = `${url.slice(0, 30)}...`
+  } else {
+    url = url.slice()
+  }
+  return url
+}
+
 export class AllBookmarks extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       path: '',
       title: ''
-      // open: false
     }
     this.getBookmarks = this.getBookmarks.bind(this)
     this.pathChanged = this.pathChanged.bind(this)
-    // this.open = this.open.bind(this)
-    // this.close = this.close.bind(this)
   }
-
-  // open = () => this.setState({ open: true })
-  // close = () => this.setState({ open: false })
 
   getBookmarks(path) {
     let category
@@ -69,20 +72,12 @@ export class AllBookmarks extends React.Component {
   render() {
     const bookmarks = this.props.bookmarks
     const title = this.state.title
-
-    return this.props.loading ? (
-      <div className="ui active loader" />
-    ) : (
+    return (
       <div onClick={this.pathChanged}>
         <Navbar />
         <div style={{display: 'flex'}}>
           <div
-            style={{
-              padding: '50px',
-              width: 0,
-              flex: 1,
-              flexDirection: 'column'
-            }}
+            style={{padding: '50px', width: 0, flex: 1, flexDirection: 'column'}}
           >
             <h1 className="title">{title}</h1>
             <AddBookmark />
@@ -92,42 +87,47 @@ export class AllBookmarks extends React.Component {
               content="Sync"
               color="teal"
             />
-            <List animated verticalAlign="middle">
-              {bookmarks && bookmarks.length > 0 ? (
-                bookmarks.map(bookmark => {
-                  return (
-                    <List.Item key={bookmark.id}>
-                      <Image
-                        avatar
-                        src={bookmark.imageUrl}
-                        className="ui image"
-                      />
-                      <List.Content>
-                        <List.Header>
-                          <a href={`//${bookmark.url}`}>{bookmark.url}</a>
-                        </List.Header>
-                      </List.Content>
-                      <Button
-                        floated="right"
-                        secondary
-                        onClick={() => {
-                          if (window.confirm('Delete this bookmark?')) {
-                            this.props.deleteBookmark(bookmark.id)
-                          }
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </List.Item>
-                  )
-                })
-              ) : (
-                <div style={{margin: '30px'}}>
-                  You don't have any bookmarks under this category!
-                </div>
-              )}
-            </List>
-          </div>
+              <List animated verticalAlign="middle">
+                {bookmarks && bookmarks.length > 0 ? (
+                  bookmarks.map(bookmark => {
+                    return (
+                      <List.Item key={bookmark.id}>
+                        <Image
+                          avatar
+                          src={bookmark.imageUrl}
+                          className="ui image"
+                        />
+                        <List.Content>
+                          <Popup
+                            content={bookmark.url}
+                            trigger={
+                              <List.Header a href={`//${bookmark.url}`}>
+                                {cleanUrl(bookmark.url)}
+                              </List.Header>
+                            }
+                          />
+                        </List.Content>
+                        <Button
+                          floated="right"
+                          secondary
+                          onClick={() => {
+                            if (window.confirm('Delete this bookmark?')) {
+                              this.props.deleteBookmark(bookmark.id)
+                            }
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </List.Item>
+                    )
+                  })
+                ) : (
+                  <div style={{margin: '30px'}}>
+                    You don't have any bookmarks under this category!
+                  </div>
+                )}
+              </List>
+            </div>
           <CustomSidebar />
         </div>
       </div>

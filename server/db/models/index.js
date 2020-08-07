447010b4937870ac8ee493cd997b166a6bc6a1ee
Jianna Park <jiannajihyunpark@gmail.com>
2020-08-07 14:48:50 -0400

    cleanup db

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,14 +3,7 @@ const Blocked = require('./blocked')
 const Goal = require('./goal')
 const Bookmark = require('./bookmark')
 const UserBlocked = require('./user_blocked')
-// const UserBookmark = require('./user_bookmark')
 const Category = require('./category')
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
 
 User.hasMany(Goal)
 Goal.belongsTo(User)
@@ -23,24 +16,15 @@ Bookmark.belongsTo(Category)
 
 User.hasMany(Bookmark)
 Bookmark.belongsTo(User)
-// User.belongsToMany(Bookmark, {through: UserBookmark})
-// Bookmark.belongsToMany(User, {through: UserBookmark})
 
 User.belongsToMany(Blocked, {through: UserBlocked})
 Blocked.belongsToMany(User, {through: UserBlocked})
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
 module.exports = {
   User,
   Blocked,
   Goal,
   Bookmark,
   UserBlocked,
-  // UserBookmark,
   Category
 }
